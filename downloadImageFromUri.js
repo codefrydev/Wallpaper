319de60beff1edb86760c33e8348dd49a1980054
codefrydev <codefrydev@gmail.com>
2024-04-12 11:31:57 +0000

    Deploying to gh-pages from  @ 1708a97801c7931b456d9ce5a4c796e763dcd0ec 🚀

---
+++ downloadImageFromUri.js
@@ -0,0 +1,16 @@
+﻿window.downloadImageUrl = (imageUrl, fileName) => {
+    fetch(imageUrl)
+        .then(response => response.blob())
+        .then(blob => {
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.style.display = 'none';
+            a.href = url;
+            // Set the file name
+            a.download = fileName;
+            document.body.appendChild(a);
+            a.click();
+            window.URL.revokeObjectURL(url);
+        })
+        .catch(error => console.error('Error downloading image:', error));
+};
